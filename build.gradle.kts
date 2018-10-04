64b92699ac25ea67c010bd8ed10c367ba6b8c29d
GDawg4 <garoz.r4@gmail.com>
2018-10-04 10:05:45 -0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    groovy
+    java
+    kotlin("jvm") version "1.2.71"
+}
+
+group = "uvg.DataBase"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile("org.codehaus.groovy:groovy-all:2.3.11")
+    compile(kotlin("stdlib-jdk8"))
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
